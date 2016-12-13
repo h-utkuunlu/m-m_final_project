d23b064e2c5e6a0c606197ff2bc3e01a1edf4c8c
h-utkuunlu <h.utkuunlu@gmail.com>
2016-12-13 15:39:03 +0400

    Final code used

--- product/main.py
+++ product/main.py
@@ -6,6 +6,7 @@ import datetime
 import imutils
 import cv2
 from time import sleep
+from random import choice
 import serial
 
 class FPS:
@@ -40,6 +41,7 @@ class WebcamVideoStream:
 	def __init__(self, src = -1):
 
 		self.stream = cv2.VideoCapture(src)
+		self.stream.set(10, 0)
 		(self.grabbed, self.frame) = self.stream.read()
 
 		self.stopped = False
@@ -104,43 +106,53 @@ def set_arm_state(mask, arm):
 
 			if center in set_on:
 				ser.write(state_list[0])
-				#cv2.circle(bg, center, 3, (255, 0, 255), -1)
+				cv2.circle(bg, center, 2, (255, 255, 255), -1)
 			else:
 				ser.write(state_list[1])
 	else:
 		ser.write(state_list[1])
 
-def update_screen(mask):
+def update_screen():
 
+	'''
 	cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 	#cnts, hierarchy = cv2.findContours(display_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	center = None
 	
 	if len(cnts) > 0:
 		for c in cnts:
-		#c = max(cnts, key=cv2.contourArea)
 			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			if radius > 3:
+			#c = max(cnts, key=cv2.contourArea)
+				((x, y), radius) = cv2.minEnclosingCircle(c)
+				M = cv2.moments(c)
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 			
-			cv2.circle(bg, center, 3, (255, 255, 255), -1)
+				#cv2.circle(bg, center, 2, (255, 255, 255), -1)
 			
-			#if radius > 2:
-				#cv2.circle(bg, (int(x), int(y)), int(radius), (0, 255, 255), 1)
-	
+				#if radius > 2:
+					#cv2.circle(bg, (int(x), int(y)), int(radius), (0, 255, 255), 1)
+	'''
 	cv2.imshow("Image", bg)
 	
+images = ["android", "battery", "cloud", "cross", "dove", "earth", "electricity", "electronics", "fire", "human", "oil", "peace", "tick", "tree", "web", "wheel"]
 
-input_file = open("images/black.txt")
+select = choice(images)
+
 set_on = set()
+input_file = open("source/" + select + ".txt")
 
 for line in input_file:
-    
-    coordinate = line.strip().split(",")
-    for i in range(2):
-        coordinate[i] = int(coordinate[i])
-    point = (coordinate[0], coordinate[1])
-    set_on.add(point)
+	
+	coordinate = line.strip().split(",")
+	for i in range(2):
+	    coordinate[i] = int(coordinate[i])
+	point = (coordinate[0], coordinate[1])
+	set_on.add(point)
+
+input_file.close()
+	
+#print(gallery)
 
 #PURPLE - HSV
 color1Lower = np.array([80, 50, 50], dtype = np.uint8)
@@ -154,7 +166,7 @@ color2Upper = np.array([80, 255, 255], dtype = np.uint8)
 #color3Lower = (100, 0, 0)
 #color3Upper = (180, 50, 255)
 
-color3Lower = np.array([0,0,180], dtype=np.uint8)
+color3Lower = np.array([0,0,200], dtype=np.uint8)
 color3Upper = np.array([0,0,255], dtype=np.uint8)
 
 ser = serial.Serial('/dev/ttyACM0', 57600)
@@ -166,18 +178,46 @@ ser.write('2'.encode())
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream().start()
 fps = FPS().start()
-bg = cv2.imread("images/bg.png")
+bg = cv2.imread("source/bg.png")
 #cv2.imshow("Frame", bg)
+counter = 0
+
+cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)          
+cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("Image", bg)
 
 while True:
 	
+	if counter > 8000:
+		select = choice(images)
+		
+		set_on = set()
+		input_file = open("source/" + select + ".txt")
+
+		for line in input_file:
+	
+			coordinate = line.strip().split(",")
+			for i in range(2):
+				coordinate[i] = int(coordinate[i])
+			point = (coordinate[0], coordinate[1])
+			set_on.add(point)
+
+		input_file.close()
+		
+		bg = cv2.imread("source/bg.png")
+		print('image change:', select)
+		counter = 0
+	
 	frame = vs.read()
+	
+	#cv2.imshow("Image", frame)
+	
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	#frame = imutils.resize(frame, width=600)
 	
 	mask1 = image_process(hsv, color1Lower, color1Upper)
 	mask2 = image_process(hsv, color2Lower, color2Upper)
-	display_mask = image_process(hsv, color3Lower, color3Upper, True)
+	#display_mask = image_process(hsv, color3Lower, color3Upper, True)
 	
 	#cv2.imshow('Purple', mask1)
 	#cv2.imshow('Yellow', mask2)
@@ -189,7 +229,7 @@ while True:
 	#ser.write('1'.encode())
 	#ser.write('3'.encode())
 	
-	update_screen(display_mask)
+	update_screen()
 	
 	key = cv2.waitKey(1) & 0xFF
 		
@@ -197,13 +237,15 @@ while True:
 		break
 		
 	if key == ord("c"):
-		bg = cv2.imread("images/bg.png")
+		bg = cv2.imread("source/bg.png")
 	fps.update()
 
 	if key == ord("f"):
 		fps.stop()
 		print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 		print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	
+	counter += 1
 
 fps.stop()
 print("[INFO] total elasped time: {:.2f}".format(fps.elapsed()))
